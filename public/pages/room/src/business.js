01798268f81ce9d40acd40a689a49cd0ab5d4415
Bruno Casas <brunocasas04@gmail.com>
2021-01-28 16:22:10 -0400

    Aula 02

--- public/pages/room/src/business.js
+++ public/pages/room/src/business.js
@@ -1,24 +1,38 @@
 class Business {
-    constructor({ room, media, view, socketBuilder }) {
+    constructor({ room, media, view, socketBuilder, peerBuilder }) {
         this.room = room
         this.media = media
         this.view = view
         this.socketBuilder = socketBuilder
-            .setOnUserConnected(this.onUserConnected())
-            .setOnUserDisconnected(this.onUserDisconnected())
-            .build()
+        this.peerBuilder = peerBuilder
 
-        this.socketBuilder.emit('join-room', this.room, 'teste01')
+        this.socket = {}
         this.currentStream = {}
-    }
+        this.currentPeer = {}
 
+        this.peers = new Map()
+    }
+    
     static initialize(deps) {
         const instance = new Business(deps)
         return instance._init()
     }
-
+    
     async _init() {
         this.currentStream = await this.media.getCamera()
+        this.socket = this.socketBuilder
+        .setOnUserConnected(this.onUserConnected())
+        .setOnUserDisconnected(this.onUserDisconnected())
+        .build()
+        
+        this.currentPeer = await this.peerBuilder
+            .setOnError(this.onPeerError())
+            .setOnConnectionOpened(this.onPeerConnectionOpened())
+            .setOnCallReceived(this.onPeerCallReceived())
+            .setOnPeerStreamReceived(this.onPeerStreamReceived())
+            .build()
+
+
         this.addVideoStream('testeEu')
     }
 
@@ -26,6 +40,7 @@ class Business {
         const isCurrentId = false
         this.view.renderVideo({
             userId,
+            muted: false,
             isCurrentId,
             stream
         })
@@ -34,6 +49,7 @@ class Business {
     onUserConnected = function() {
         return userId => {
             console.log('user connected!', userId);
+            this.currentPeer.call(userId, this.currentStream)
         }
     }
 
@@ -43,4 +59,34 @@ class Business {
         }
     }
 
+    onPeerError = function() {
+        return error => {
+            console.error('Error on peer', error)
+        }
+    }
+
+    onPeerConnectionOpened = function(params) {
+        return peer => {
+            const { id } = peer
+            console.log('peer!!', peer);
+            this.socket.emit('join-room', this.room, id)
+        }
+    }
+
+    onPeerCallReceived = function() {
+        return call => {
+            console.log('answeing call', call)
+            call.answer(this.currentStream)
+        }
+    }
+
+    onPeerStreamReceived = function() {
+        return ( call, stream ) => {
+            const callerId = call.peer
+            this.addVideoStream(callerId, stream)
+
+            this.peers.set(callerId, { call })
+            this.view.setParticipants(this.peers.size)
+        }
+    }
 }
